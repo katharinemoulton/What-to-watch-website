89e2150f2f5f87504c784d0db6613faa63daba3c
Vithica <sinister.vithica@gmail.com>
2018-04-12 00:15:02 +0100

    Added some javascript

---
+++ film_script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+$(".btn-twitter").click(function() {
+alert("Thank you for sharing this page!");
+});
+$(".btn-facebook").click(function() {
+alert("Thank you for sharing this page!");
+});
+$(".btn-email").click(function() {
+alert("Thank you for sharing this page!");
+});
+$(".submit").click(function() {
+alert("Thank you for sharing your opinion! It will help make our recommendations better! ");
+});
+});
